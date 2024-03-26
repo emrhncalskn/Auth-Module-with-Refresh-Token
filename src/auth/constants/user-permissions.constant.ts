9e40f4ba6d17da9c67402ecba7f844a1eb5909d1
emir <emrhncalskn@gmail.com>
2024-03-26 15:59:53 +0300

    Add user permissions and provide explanation

--- src/auth/constants/user-permissions.constant.ts
+++ src/auth/constants/user-permissions.constant.ts
@@ -5,3 +5,8 @@ export class UserPermissionsConstant {
             { path: '/test', method: 'get' },
         ]
 }
+
+// Bu constant içerisinde kullanıcı rolüne verilecek izinler tanımlanır.
+// Buraya eklenen izinler, kullanıcı rolüne default olarak atanır.
+// Uygulama bir kere çalıştıktan sonra buradan silinen izinler silinmez, veritabanından silinmesi gerekmektedir.
+// Burası haricinde /permission apileri üzerinden izinler yönetilebilir. 
